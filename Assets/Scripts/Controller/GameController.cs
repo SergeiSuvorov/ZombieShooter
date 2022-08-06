ca9f8b237cb994095f241b6e89978d2de15821ef
SergeiSuvorov <78319712+SergeiSuvorov@users.noreply.github.com>
2022-08-06 16:00:17 +0300

    Add GameMapLoader

---
+++ Assets/Scripts/Controller/GameController.cs
@@ -0,0 +1,17 @@
+﻿using Tools;
+using UnityEngine;
+
+
+namespace Controller
+{
+    public class GameController : BaseController
+    {
+        private MapController _mapController;
+        public GameController()
+        {
+            var MoveDiff = new SubscriptionProperty<Vector2>();
+            _mapController = new MapController();
+        }
+    }
+}
+
